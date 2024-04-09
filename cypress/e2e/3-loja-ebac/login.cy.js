b828e1526c48a927519d521471cbb0a94456d50a
pedro gomes <pedro.gonka02@gmail.com>
2024-04-09 10:34:46 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,13 @@
+///<reference types="cypress"/>
+
+describe ('funcionalidade: Login', () => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('pedro01teste@teste.com')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, pedro01teste (não é pedro01teste? Sair)')
+    })
+  })
+
